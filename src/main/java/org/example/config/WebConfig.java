6c47e79a83ab620668761c3bb467d5e70cb95672
OanaSpatacean <126173446+OanaSpatacean@users.noreply.github.com>
2024-12-11 15:55:12 +0200

    Deploy preparation

--- src/main/java/org/example/config/WebConfig.java
+++ src/main/java/org/example/config/WebConfig.java
@@ -2,6 +2,7 @@ package org.example.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,5 +14,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{spring:[^.]*}").setViewName("forward:/index.html");
     }
 }
